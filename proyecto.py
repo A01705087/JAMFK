e4d7b1de9c1f4860d135ff06e2f11d0770d9bc85
A01705087 <88898328+A01705087@users.noreply.github.com>
2021-08-27 12:59:22 -0500

    Add files via upload

--- proyecto.py
+++ proyecto.py
@@ -22,16 +22,35 @@ if opcion==0:
 elif opcion==1:
     modo_de_juego="resta"
 
+resultado=0
+
+respuesta=0
+
 #Inicio del juego y juego
 
 if opcion==0:
     print('Bienvenid@ a la suma')
     print('Ahora contestará diez preguntas')
     print('Recuerde que tiene tres vidas')
+    respuesta=int(input('¿Cuál es el resultado de 2+2 ?'))
+    resultado= c+2*b
+    if respuesta == resultado:
+        print('Tienes la respuesta correcta')
+    else:
+        print('Tuviste la respuesta incorrecta')
+        contador_de_vidas= contador_de_vidas-1
+
 elif opcion==1:
     print('Bienvenid@ a la resta')
     print('Ahora contestarás diez preguntas')
     print('Recuerda que tienes tres vidas')
+    respuesta=int(input('¿Cuál es el resultado de 2-2 ?'))
+    resultado= c-2*b
+    if respuesta == resultado:
+        print('Tienes la respuesta correcta')
+    else:
+        print('Tuviste la respuesta incorrecta')
+        contador_de_vidas= contador_de_vidas-1
 else:
     print('Esa no es una opción válida')
 
