492e6b052fac66ff3de76f16ec05f2e0cefb3a75
A01705087 <88898328+A01705087@users.noreply.github.com>
2021-08-31 15:25:05 -0500

    Add files via upload

--- proyecto.py
+++ proyecto.py
@@ -6,53 +6,96 @@ print('Escoge el tipo de operación')
 print('Suma,\nResta')
 print('Para suma escoja 0, \ny para resta 1')
 
-#Asignar variantes y creación de bancos de preguntas
+#Asignar variantes, constantes y creación de bancos de preguntas
 
-opcion=int(input('Escoja su número por favor'))
 contador_de_vidas=3
 
 modo_de_juego="suma"
 
 a=0
 b=1
-c=2
+
+acum=1
+
+resultado=0
+
+respuesta=0
+
+# Elección
+
+opcion=int(input('Escoja su número por favor'))
+
+if opcion>2:
+    print('No ha escogido una opción válida')
+    opcion = int(input('Por favor hágalo de nuevo, \n Suma es 0, \n y resta 1'))
+else:
+    print('Muy bien. Comencemos el juego.')
+
 
 if opcion==0:
     modo_de_juego="suma"
 elif opcion==1:
     modo_de_juego="resta"
 
-resultado=0
+#Funciones del juego
 
-respuesta=0
+def generar_pregunta(valor_1, valor_2):
+    uno=str(valor_1)
+    dos=str(valor_2)
+    pregunta=uno,'+', dos
+    return pregunta
 
-#Inicio del juego y juego
+def crear_respuesta(valor1, valor2):
+    answer=valor1+valor2
+    return answer
+
+def generar_pregunta2(valor_1, valor_2):
+    uno=str(valor_1)
+    dos=str(valor_2)
+    pregunta=uno,'-', dos
+    return pregunta
+
+def crear_respuesta2(valor1, valor2):
+    answer= valor1-valor2
+    return answer
+
+# Juego
 
 if opcion==0:
     print('Bienvenid@ a la suma')
     print('Ahora contestará diez preguntas')
     print('Recuerde que tiene tres vidas')
-    respuesta=int(input('¿Cuál es el resultado de 2+2 ?'))
-    resultado= c+2*b
+    print('Pregunta',acum)
+    print('¿Cuál es el resultado de', generar_pregunta(53,3), '?')
+    print('Ponga su respuesta')
+    resultado= crear_respuesta(53, 3)
+    respuesta=int(input())
     if respuesta == resultado:
         print('Tienes la respuesta correcta')
     else:
         print('Tuviste la respuesta incorrecta')
         contador_de_vidas= contador_de_vidas-1
+    acum=acum+1
+    a=a+1
+    b=b+1
 
 elif opcion==1:
     print('Bienvenid@ a la resta')
     print('Ahora contestarás diez preguntas')
     print('Recuerda que tienes tres vidas')
-    respuesta=int(input('¿Cuál es el resultado de 2-2 ?'))
-    resultado= c-2*b
-    if respuesta == resultado:
-        print('Tienes la respuesta correcta')
-    else:
-        print('Tuviste la respuesta incorrecta')
-        contador_de_vidas= contador_de_vidas-1
-else:
-    print('Esa no es una opción válida')
+    print('Pregunta',acum)
+    print('¿Cuál es el resultado de', generar_pregunta2(100,25), '?')
+        print('Ponga su respuesta')
+        resultado= crear_respuesta2(100, 25)
+        respuesta=int(input())
+        if respuesta == resultado:
+            print('Tienes la respuesta correcta')
+        else:
+            print('Tuviste la respuesta incorrecta')
+            contador_de_vidas= contador_de_vidas-1
+        acum=acum+1
+        a=a+1
+        b=b+1
 
 
 #Conclusión
@@ -62,5 +105,6 @@ if contador_de_vidas>0:
     print('Con:', contador_de_vidas, 'vidas.')
     print('Gracias por jugar JMFK. :)')
 else:
+    print('Game Over!')
     print('Lo siento.')
     print('Puedes tener mejor suerte para la próxima.')
