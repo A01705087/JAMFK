58abeb2ae1e87ccdfe1612307062d5ba0b452c57
A01705087 <88898328+A01705087@users.noreply.github.com>
2021-09-28 08:46:43 -0500

    Add files via upload

--- proyecto.py
+++ proyecto.py
@@ -2,22 +2,26 @@
 #Funciones del juego
 
 def generar_pregunta(valor_1, valor_2):
+    """" Genera la pregunta para la suma."""
     uno= str(valor_1)
     dos= str(valor_2)
     pregunta= uno,'+', dos
     return pregunta
 
 def crear_respuesta(valor1, valor2):
+    """" Se hace la operación aritmética de los números."""
     answer= valor1 + valor2
     return answer
 
 def generar_pregunta2(valor_1, valor_2):
+    """" Obtiene la pregunta para la resta."""
     uno= str(valor_1)
     dos= str(valor_2)
     pregunta= uno,'-', dos
     return pregunta
 
 def crear_respuesta2(valor1, valor2):
+    """ De los números de la lista, se hace resta."""
     answer= valor1 - valor2
     return answer
 
@@ -36,6 +40,10 @@ modo_de_juego="suma"
 
 a=0
 b=1
+
+lista_suma=[23,56,74,38,89,87,27,10,45,2,3,91,22,34,57,101,99,76,54, 67,5]
+
+lista_resta=[32,12,43,53,92,22,33,76,56,10,5,3,2,8,90,57,98,100,64,73,145,173]
 
 acum=1
 
@@ -62,9 +70,9 @@ if opcion==0:
     print('Recuerde que tiene tres vidas')
     while contador_de_vidas > 0 and acum <= 10:
         print('Pregunta',acum)
-        print('¿Cuál es el resultado de', generar_pregunta(53,3), '?')
+        print('¿Cuál es el resultado de', generar_pregunta(lista_suma[a],lista_suma[b]), '?')
         print('Ponga su respuesta')
-        resultado= crear_respuesta(53, 3)
+        resultado= crear_respuesta(lista_suma[a], lista_suma[b])
         respuesta= int(input())
         if respuesta == resultado:
             print('Tienes la respuesta correcta')
@@ -73,7 +81,7 @@ if opcion==0:
             contador_de_vidas= contador_de_vidas - 1
         acum= acum + 1
         a= a + 1
-        b= b + 1
+        b= b + 2
 
 elif opcion==1:
     modo_de_juego = "resta"
@@ -82,9 +90,9 @@ elif opcion==1:
     print('Recuerda que tienes tres vidas')
     while contador_de_vidas > 0 and acum <= 10:
         print('Pregunta',acum)
-        print('¿Cuál es el resultado de', generar_pregunta2(100,25), '?')
+        print('¿Cuál es el resultado de', generar_pregunta2(lista_resta[a],lista_resta[b]), '?')
         print('Ponga su respuesta')
-        resultado= crear_respuesta2(100, 25)
+        resultado= crear_respuesta2(lista_resta[a], lista_resta[b])
         respuesta= int(input())
         if respuesta == resultado:
             print('Tienes la respuesta correcta')
@@ -93,7 +101,7 @@ elif opcion==1:
             contador_de_vidas= contador_de_vidas - 1
         acum= acum + 1
         a= a + 1
-        b= b + 1
+        b= b + 2
 
 
 #Conclusión
