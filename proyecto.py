a178d92f4e86dd96b997a08b60c76e8e9f55e718
A01705087 <88898328+A01705087@users.noreply.github.com>
2021-10-05 10:53:22 -0500

    Add files via upload

--- proyecto.py
+++ proyecto.py
@@ -27,29 +27,31 @@ def crear_respuesta2(valor1, valor2):
 
 #Inicio del programa
 
-print('Bienvenid@ a JMFK, un juego divertido para practicar matemáticas')
+print('Bienvenid@ a JMFK,'
+      '\n un juego divertido para practicar matemáticas')
 print('Escoge el tipo de operación')
 print('Suma,\nResta')
-print('Para suma escoja 0, \ny para resta 1')
+print('Para suma escoja 0, '
+      '\ny para resta 1')
 
 #Asignar variantes, constantes y creación de bancos de preguntas
 
-contador_de_vidas=3
+contador_de_vidas= 3
 
-modo_de_juego="suma"
+modo_de_juego= "suma"
 
-a=0
-b=1
+a= 0
+b= 1
 
-lista_suma=[23,56,74,38,89,87,27,10,45,2,3,91,22,34,57,101,99,76,54, 67,5]
+lista_suma=[23,56,74,38,89,87,27,10,45,2,3,91,22,34,57,101,99,76,54,67]
 
-lista_resta=[32,12,43,53,92,22,33,76,56,10,5,3,2,8,90,57,98,100,64,73,145,173]
+lista_resta=[32,12,43,53,92,22,33,76,56,10,5,3,2,8,90,57,98,100,64,73]
 
-acum=1
+acum= 1
 
-resultado=0
+resultado= 0
 
-respuesta=0
+respuesta= 0
 
 # Elección
 
@@ -57,7 +59,9 @@ opcion=int(input('Escoja su número por favor'))
 
 while opcion>=2:
     print('No ha escogido una opción válida')
-    opcion= int(input('Por favor hágalo de nuevo, \n Suma es 0, \n y resta 1'))
+    opcion= int(input('Por favor hágalo de nuevo,'
+                      ' \n Suma es 0, '
+                      '\n y resta 1'))
 else:
     print('Muy bien. Comencemos el juego.')
 
@@ -70,9 +74,12 @@ if opcion==0:
     print('Recuerde que tiene tres vidas')
     while contador_de_vidas > 0 and acum <= 10:
         print('Pregunta',acum)
-        print('¿Cuál es el resultado de', generar_pregunta(lista_suma[a],lista_suma[b]), '?')
+        print('¿Cuál es el resultado de',
+              generar_pregunta(lista_suma[a],
+                               lista_suma[b]), '?')
         print('Ponga su respuesta')
-        resultado= crear_respuesta(lista_suma[a], lista_suma[b])
+        resultado= crear_respuesta(lista_suma[a],
+                                   lista_suma[b])
         respuesta= int(input())
         if respuesta == resultado:
             print('Tienes la respuesta correcta')
@@ -90,9 +97,12 @@ elif opcion==1:
     print('Recuerda que tienes tres vidas')
     while contador_de_vidas > 0 and acum <= 10:
         print('Pregunta',acum)
-        print('¿Cuál es el resultado de', generar_pregunta2(lista_resta[a],lista_resta[b]), '?')
+        print('¿Cuál es el resultado de',
+              generar_pregunta2(lista_resta[a],
+                                lista_resta[b]), '?')
         print('Ponga su respuesta')
-        resultado= crear_respuesta2(lista_resta[a], lista_resta[b])
+        resultado= crear_respuesta2(lista_resta[a],
+                                    lista_resta[b])
         respuesta= int(input())
         if respuesta == resultado:
             print('Tienes la respuesta correcta')
@@ -107,7 +117,8 @@ elif opcion==1:
 #Conclusión
 
 if contador_de_vidas>0:
-    print('¡Felicidades! Haz logrado contestar las diez preguntas de', modo_de_juego,'.')
+    print('¡Felicidades! Haz logrado contestar las diez preguntas de',
+          modo_de_juego,'.')
     print('Con:', contador_de_vidas, 'vidas.')
     print('Gracias por jugar JMFK. :)')
 else:
